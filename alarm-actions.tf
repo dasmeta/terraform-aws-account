212170d47da9da85bbe58793560d1428f05b7105
Aram Karapetyan <aram@dasmeta.com>
2024-11-12 11:20:14 +0400

    fix(DMVP-5761): Update SNS module for support Budget policy

--- alarm-actions.tf
+++ alarm-actions.tf
@@ -1,44 +1,3 @@
-locals {
-  policy = {
-    "Version" : "2008-10-17",
-    "Id" : "__default_policy_ID",
-    "Statement" : [
-      {
-        "Sid" : "__default_statement_ID",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "*"
-        },
-        "Action" : [
-          "SNS:GetTopicAttributes",
-          "SNS:SetTopicAttributes",
-          "SNS:AddPermission",
-          "SNS:RemovePermission",
-          "SNS:DeleteTopic",
-          "SNS:Subscribe",
-          "SNS:ListSubscriptionsByTopic",
-          "SNS:Publish"
-        ],
-        "Resource" : "arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:${var.alarm_actions.topic_name}",
-        "Condition" : {
-          "StringEquals" : {
-            "AWS:SourceOwner" : "${data.aws_caller_identity.current.account_id}"
-          }
-        }
-      },
-      {
-        "Sid" : "AWSBudgets-notification-1",
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "budgets.amazonaws.com"
-        },
-        "Action" : "SNS:Publish",
-        "Resource" : "*"
-      }
-    ]
-  }
-}
-
 module "cloudwatch_alarm_actions" {
   source  = "dasmeta/monitoring/aws//modules/cloudwatch-alarm-actions"
   version = "1.19.3"
@@ -53,7 +12,7 @@ module "cloudwatch_alarm_actions" {
   servicenow_webhooks      = var.alarm_actions.servicenow_webhooks
   teams_webhooks           = var.alarm_actions.teams_webhooks
   enable_dead_letter_queue = var.alarm_actions.enable_dead_letter_queue
-  policy                   = local.policy
+  policy                   = local.sns_access_policy
 }
 
 module "cloudwatch_alarm_actions_virginia" {
@@ -70,7 +29,7 @@ module "cloudwatch_alarm_actions_virginia" {
   servicenow_webhooks      = var.alarm_actions_virginia.servicenow_webhooks
   teams_webhooks           = var.alarm_actions_virginia.teams_webhooks
   enable_dead_letter_queue = var.alarm_actions_virginia.enable_dead_letter_queue
-  policy                   = local.policy
+  policy                   = local.sns_access_policy
 
   providers = {
     aws = aws.virginia
