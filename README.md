c065d8cc8666a97f84cf889463d4875900e2be91
Aram Karapetyan <aram@dasmeta.com>
2024-11-13 13:28:56 +0400

    fix(DMVP-5761): Update module policy

--- README.md
+++ README.md
@@ -47,6 +47,7 @@ module "account" {
 | Name | Type |
 |------|------|
 | [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
+| [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
 
 ## Inputs
 
--- locals.tf
+++ locals.tf
@@ -19,7 +19,7 @@ locals {
           "SNS:ListSubscriptionsByTopic",
           "SNS:Publish"
         ],
-        "Resource" : "arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:${var.alarm_actions.topic_name}",
+        "Resource" : "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.alarm_actions.topic_name}",
         "Condition" : {
           "StringEquals" : {
             "AWS:SourceOwner" : "${data.aws_caller_identity.current.account_id}"
@@ -34,6 +34,24 @@ locals {
         },
         "Action" : "SNS:Publish",
         "Resource" : "*"
+      },
+      {
+        "Sid" : "CloudWatch-Alarms-Publish",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:GetTopicAttributes",
+          "SNS:SetTopicAttributes",
+          "SNS:AddPermission",
+          "SNS:RemovePermission",
+          "SNS:DeleteTopic",
+          "SNS:Subscribe",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:Publish"
+        ],
+        "Resource" : "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.alarm_actions.topic_name}"
       }
     ]
   }
